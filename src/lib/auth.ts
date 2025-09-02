6564bc32c7bae0b395bda50363cd58ed7e533ea3
alfre755 <4lfr3755@gmail.com>
2025-09-02 00:33:17 -0400

    Feat: New libs
    Se agrega betterauth, drizzle, y postgres con sus respectivas configuraciones iniciales

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db"; // your drizzle instance
+import { organization } from "better-auth/plugins";
+import { admin } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+  }),
+  plugins: [organization(), admin()],
+});
