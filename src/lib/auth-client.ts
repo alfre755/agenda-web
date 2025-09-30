ad93131a97ca6583eae0be0b0435787d6f28bb7f
alfre755 <4lfr3755@gmail.com>
2025-09-29 22:04:02 -0300

    Fix: Base url
    para build

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,7 +5,7 @@ import { ac, roles } from "@/lib/permissions";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"),
   plugins: [
     adminClient({ ac, roles }),
     organizationClient(),
